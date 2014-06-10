740639b3be7a70f44070f5a468df477320e83c08
simpleframework <cknet@126.com>
2014-06-10 23:51:26 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/dict/DictItem.java
+++ src/main/java/net/simpleframework/module/dict/DictItem.java
@@ -18,22 +18,20 @@ public class DictItem extends AbstractDict implements IDateAwareBean {
 
 	private ID dictId;
 
-	/**
-	 * 编码
-	 */
+	/* 编码 */
 	private String codeNo;
 
 	private EDictItemMark itemMark;
-
-	/**
-	 * 字典条目的创建日期
-	 */
+	/* 字典条目的创建日期 */
 	private Date createDate;
 
-	/**
-	 * 字典条目的拥有人，null为全局
-	 */
+	/* 字典条目的拥有人，null为全局 */
 	private ID userId;
+
+	/* 扩展字段1 */
+	private String ext1;
+	/* 扩展字段2 */
+	private int ext2;
 
 	public ID getDictId() {
 		return dictId;
@@ -75,6 +73,22 @@ public class DictItem extends AbstractDict implements IDateAwareBean {
 
 	public void setUserId(final ID userId) {
 		this.userId = userId;
+	}
+
+	public String getExt1() {
+		return ext1;
+	}
+
+	public void setExt1(final String ext1) {
+		this.ext1 = ext1;
+	}
+
+	public int getExt2() {
+		return ext2;
+	}
+
+	public void setExt2(final int ext2) {
+		this.ext2 = ext2;
 	}
 
 	private static final long serialVersionUID = 5683629062027025972L;
