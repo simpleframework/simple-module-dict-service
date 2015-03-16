604870f13f65c7bd7de673395e2c25980ad09c57
CHENKAN <CHENKAN@CHENKAN-PC>
2015-03-16 11:29:21 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/module/dict/impl/DictContext.java
+++ src/main/java/net/simpleframework/module/dict/impl/DictContext.java
@@ -33,8 +33,17 @@ public class DictContext extends AbstractADOModuleContext implements IDictContex
 
 	@Override
 	protected Module createModule() {
-		return new Module().setManagerRole(ROLE_DICT_MANAGER).setRole(ROLE_DICT_MANAGER)
-				.setName(MODULE_NAME).setText($m("DictContext.0")).setOrder(35);
+		return new Module() {
+			@Override
+			public String getManagerRole() {
+				return ROLE_DICT_MANAGER;
+			}
+
+			@Override
+			public String getRole() {
+				return ROLE_DICT_MANAGER;
+			}
+		}.setName(MODULE_NAME).setText($m("DictContext.0")).setOrder(35);
 	}
 
 	@Override
