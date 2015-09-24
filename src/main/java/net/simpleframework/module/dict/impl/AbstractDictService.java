05141da5abcf45956d479b1721b57740a1932ff4
CHENK <CHENK@CHENK-PC>
2015-09-24 18:39:47 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/dict/impl/AbstractDictService.java
+++ src/main/java/net/simpleframework/module/dict/impl/AbstractDictService.java
@@ -1,5 +1,7 @@
 package net.simpleframework.module.dict.impl;
 
+import java.io.Serializable;
+
 import net.simpleframework.ctx.IModuleContext;
 import net.simpleframework.ctx.service.ado.db.AbstractDbBeanService;
 import net.simpleframework.module.dict.IDictContextAware;
@@ -10,8 +12,8 @@ import net.simpleframework.module.dict.IDictContextAware;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public abstract class AbstractDictService<T> extends AbstractDbBeanService<T> implements
-		IDictContextAware {
+public abstract class AbstractDictService<T extends Serializable> extends AbstractDbBeanService<T>
+		implements IDictContextAware {
 
 	@Override
 	public IModuleContext getModuleContext() {
