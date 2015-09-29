f13cbc533daf755c375989e91a742fc85bc62b8d
CHENK <CHENK@CHENK-PC>
2015-09-29 20:03:47 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/dict/impl/DefaultDictImportHandler.java
+++ src/main/java/net/simpleframework/module/dict/impl/DefaultDictImportHandler.java
@@ -3,7 +3,7 @@ package net.simpleframework.module.dict.impl;
 import net.simpleframework.common.ClassUtils;
 import net.simpleframework.common.object.ObjectFactory;
 import net.simpleframework.ctx.IApplicationContext;
-import net.simpleframework.ctx.common.IDataImportHandler;
+import net.simpleframework.ctx.hdl.AbstractDataImportHandler;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -11,7 +11,7 @@ import net.simpleframework.ctx.common.IDataImportHandler;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class DefaultDictImportHandler implements IDataImportHandler {
+public class DefaultDictImportHandler extends AbstractDataImportHandler {
 	@Override
 	public boolean isEnable() {
 		return false;
--- src/main/java/net/simpleframework/module/dict/impl/DictContext.java
+++ src/main/java/net/simpleframework/module/dict/impl/DictContext.java
@@ -3,7 +3,6 @@ package net.simpleframework.module.dict.impl;
 import static net.simpleframework.common.I18n.$m;
 import net.simpleframework.ado.ColumnData;
 import net.simpleframework.ado.db.DbEntityTable;
-import net.simpleframework.ado.db.IDbEntityTableRegistry;
 import net.simpleframework.ctx.AbstractADOModuleContext;
 import net.simpleframework.ctx.IModuleRef;
 import net.simpleframework.ctx.Module;
@@ -19,12 +18,11 @@ import net.simpleframework.module.dict.IDictService;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class DictContext extends AbstractADOModuleContext implements IDictContext,
-		IDbEntityTableRegistry {
+public class DictContext extends AbstractADOModuleContext implements IDictContext {
 	public static String ROLE_DICT_MANAGER;
 
 	@Override
-	public DbEntityTable[] createEntityTables() {
+	protected DbEntityTable[] createEntityTables() {
 		return new DbEntityTable[] {
 				new DbEntityTable(Dict.class, "sf_dict"),
 				new DbEntityTable(DictItem.class, "sf_dict_item").setDefaultOrder(ColumnData
